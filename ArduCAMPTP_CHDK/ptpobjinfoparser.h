4b61a4f3507c47a9897860062a20863eaf47bc36
sandrobenigno <sbenigno@gmail.com>
2015-03-22 22:38:35 -0300

    GPLv2+
    Moved from GPLv2 to GPLv2+.

--- ArduCAMPTP_CHDK/ptpobjinfoparser.h
+++ ArduCAMPTP_CHDK/ptpobjinfoparser.h
@@ -19,7 +19,7 @@ Note: The PTP library used here was modified by the author.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
+as published by the Free Software Foundation; either version 2+
 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
